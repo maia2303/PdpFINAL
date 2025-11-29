32e398d6b4dde1f8e847c82283e409c2410b4a88
maia2303 <bulaciomaia23@gmail.com>
2025-11-29 17:36:52 -0300

    agregue objeto tarea, detalle y main

---
+++ src/models/Tarea.ts
@@ -0,0 +1,14 @@
+export type Estado = "pendiente" | "en curso" | "terminada" | "cancelada"; 
+export type Dificultad = 1 | 2 | 3;
+
+export interface Tarea {
+    readonly id: number,
+    readonly titulo: string;
+    readonly descripcion: string;
+    readonly estado: Estado;
+    readonly dificultad: Dificultad;
+    readonly vencimiento: string | null;
+    readonly creacion: string; //en los datos la creacion y ultima edicion se declaran como string y despues pasan a nueva fecha
+    readonly ultimaEdicion: string;
+    readonly eliminada: boolean; // indica si la tarea esta marcada como eliminada (Soft delete) 
+}
