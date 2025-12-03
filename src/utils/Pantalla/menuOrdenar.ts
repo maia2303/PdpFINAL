52e98252076de2f8ad88ec00f50ba70d2aac9e63
Ange Garavaglia <garavagliaangelina81@gmail.com>
2025-12-03 09:56:20 -0300

    Ordenamiento

--- src/utils/Pantalla/menuOrdenar.ts
+++ src/utils/Pantalla/menuOrdenar.ts
@@ -1,10 +1,10 @@
 import PromptSync from "prompt-sync";
 import { GestorTareas } from "../GestorTareas";
-import { ordenCriterio } from "../FuncionesPuras/ordenarTareas";
+import { ordenarTarea, ordenCriterio, Orden } from "../FuncionesPuras/ordenarTareas";
 import { menuVerTarea } from "./menuVerTarea";
+import { Tarea } from "../../models/Tarea";
 
 const prompt = PromptSync();
-
 
 export const menuOrdenar = (gestor: GestorTareas) => {
             console.log("\n ORDENAR TAREAS ");
@@ -15,10 +15,36 @@ export const menuOrdenar = (gestor: GestorTareas) => {
             console.log(" - dificultad");
 
         const criterioInput = prompt("Escribe el criterio: ").toLowerCase();
-            if(["titulo", "vencimiento", "creacion", "prioridad"].includes(criterioInput)){
+            if(["titulo", "vencimiento", "creacion", "dificultad"].includes(criterioInput)){
                 gestor.ordenar(criterioInput as ordenCriterio);
-                        console.log("Lista ordenada con exito!");
+                        console.log("✅Lista ordenada con exito!");
             } else {
-                    console.log("Criterio invalido, intenta nuevamente.");
+                    console.log("❌Criterio invalido, intenta nuevamente.");
             }
+
+            const criterio = criterioInput as ordenCriterio;
+
+            //pedir orden 
+            const ordenInput = prompt("Orden (asc/desc): ").toLowerCase();
+    const orden: Orden = ordenInput === "desc" ? "desc" : "asc"; // valor por defecto asc
+
+            //obtener tareas ordenadas
+            const lista = gestor.getTarea();
+
+            if (lista.length === 0) {
+                console.log("❌No hay tareas para ordenar."); 
+                return;
+            }
+
+            const listaOrdenada = ordenarTarea(lista, criterio, orden);
+
+            //mostar/ordenar usando el criterio y orden
+    console.log(`\n📌 Tareas ordenadas por "${criterio}" (${orden})`);
+    listaOrdenada.forEach((t: Tarea) => {
+        console.log(`- [${t.id}] ${t.titulo} | vence: ${t.vencimiento} | dif: ${t.dificultad}`);
+
+        console.log("\n✅ Ordenamiento completado.");
+    });
+
+
 }
